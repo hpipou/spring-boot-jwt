0bf6868d67695f106df3741033c54aea900bc57b
hpipou <mr.aoudjit@gmail.com>
2022-09-09 08:55:34 +0200

    initialisation du projet

---
+++ src/main/java/io/jwt/refreshtoken/RefreshtokenApplication.java
@@ -0,0 +1,13 @@
+package io.jwt.refreshtoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefreshtokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefreshtokenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/jwt/refreshtoken/RefreshtokenApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jwt.refreshtoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefreshtokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
